df13f87c630b50a7a808a9d59427c75af1f53bfa
Eduardo2486 <eduardo0137@protonmail.com>
2023-03-06 22:29:57 -0600

    Globant

---
+++ 2. Types and Operators.js
@@ -0,0 +1,20 @@
+// Primitive Types
+// A type that represents a single value
+
+// undefined = Lack of existence
+// null = Lack of existence
+
+// do not set undefined to a variable but you can use null
+
+// bool
+// number
+// string - can use '' or ""
+// symbol 
+
+
+//Operators
+// Read this article, too long to make a short version of this
+// https://www.w3schools.blog/operator-precedence-in-javascript
+
+//Default values
+// var a = someVar || "Default";
---
+++ 3. Objects and Functions.js
@@ -0,0 +1,14 @@
+// {} is known as object literal
+
+// Functions are objects
+// First class functions: Everything you can do with other types  
+// you can do with functions
+
+// Function: a special type of object
+
+function greet() {
+  console.log("Hi!");
+}
+
+greet.language = "English";
+console.log(greet.language);
