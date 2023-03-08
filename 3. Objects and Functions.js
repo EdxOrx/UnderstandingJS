217d3deb224a6947d68f347a52749cb387e452f2
Eduardo2486 <eduardo0137@protonmail.com>
2023-03-07 21:58:32 -0600

    Globant

--- 3. Objects and Functions.js
+++ 3. Objects and Functions.js
@@ -7,8 +7,43 @@
 // Function: a special type of object
 
 function greet() {
-  console.log("Hi!");
+  console.log('Hi!');
 }
 
-greet.language = "English";
+greet.language = 'English';
 console.log(greet.language);
+
+
+// Expressions:
+// Unit of code that results in a value
+
+// Statement:
+// programming instructions
+
+
+// Pass by value (primitives)
+var a = 3;
+var b;
+
+b = a;
+a = 2;
+
+console.log(a);
+console.log(b);
+
+// Pass by reference 
+var c = {greeting: 'hi'};
+var d;
+
+d = c;
+
+c.greeting = 'Hello';
+
+console.log(c);
+console.log(d);
+
+// Callback: function passed to another function to execute
+
+// bind: helps to point to another object with using 'this'
+// call: helps to point to another object with using 'this' and execute but you have to pass the arguments one by one
+// apply: helps to point to another object with using 'this' and execute but you have to pass the arguments in an array
